7873684efe70cd6e5e440f31d93080e5ef9634e5
toicodemoingay <toicodemoingay@example.com>
2022-10-08 16:25:47 +0200

    Create a BaseballCoach class

---
+++ testproject/src/testproject/BaseballCoach.java
@@ -0,0 +1,17 @@
+package testproject;
+
+public class BaseballCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
