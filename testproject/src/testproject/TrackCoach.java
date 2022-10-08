5e9df8b173e8a9737443045f5c9f5392ba7d458e
toicodemoingay <toicodemoingay@example.com>
2022-10-08 16:38:20 +0200

    Create a TrachCoach class

---
+++ testproject/src/testproject/TrackCoach.java
@@ -0,0 +1,17 @@
+package testproject;
+
+public class TrackCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
